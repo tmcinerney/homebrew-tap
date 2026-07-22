25ee98f726450cec143d79f185226fb909805dd2
Travers McInerney <trav@quickture.com>
2026-07-22 15:28:55 -0700

    granola-cli 0.2.0

--- Formula/granola-cli.rb
+++ Formula/granola-cli.rb
@@ -1,24 +1,24 @@
 class GranolaCli < Formula
   desc "Unofficial Granola CLI with desktop 7.427+ refresh bootstrap"
   homepage "https://github.com/tmcinerney/granola-cli"
-  version "0.1.4"
+  version "0.2.0"
   license "MIT"
 
   on_macos do
     on_arm do
-      url "https://github.com/tmcinerney/granola-cli/releases/download/v0.1.4/granola-v0.1.4-aarch64-apple-darwin.tar.gz"
-      sha256 "5c8f3da3b50834981501c7e0829cf3d4c9c8ac89c2c42bc7de8b61be4cf58fc4"
+      url "https://github.com/tmcinerney/granola-cli/releases/download/v0.2.0/granola-v0.2.0-aarch64-apple-darwin.tar.gz"
+      sha256 "51e9c6782c2a72d61d03d2a8fb45259f998344a4bee514dad3230085a83b7a08"
     end
     on_intel do
-      url "https://github.com/tmcinerney/granola-cli/releases/download/v0.1.4/granola-v0.1.4-x86_64-apple-darwin.tar.gz"
-      sha256 "708c8d8866675f46435ab9ff4692159cec3cd332067e259f1962d8949c245739"
+      url "https://github.com/tmcinerney/granola-cli/releases/download/v0.2.0/granola-v0.2.0-x86_64-apple-darwin.tar.gz"
+      sha256 "943759c8a933c566d2d417f00bedc5e01e17c631c63c4595940466bcb8d63fdc"
     end
   end
 
   on_linux do
     on_intel do
-      url "https://github.com/tmcinerney/granola-cli/releases/download/v0.1.4/granola-v0.1.4-x86_64-unknown-linux-gnu.tar.gz"
-      sha256 "9cec150578de2057481acd9a9d270bd9b15e305135a3234d3ab177641c41b6f6"
+      url "https://github.com/tmcinerney/granola-cli/releases/download/v0.2.0/granola-v0.2.0-x86_64-unknown-linux-gnu.tar.gz"
+      sha256 "ab090e132affaa2cb824eb2457acdf9600f5ee8d30344717e2ab332e79caa144"
     end
   end
 
